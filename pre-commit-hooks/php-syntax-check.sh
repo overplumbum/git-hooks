404214d1b727b5f237139ee39e8bfdd137a62230
Denis Orlikhin <qbikk@yandex.ru>
2010-09-13 17:43:12 +0400

    some fixes

--- pre-commit-hooks/php-syntax-check.sh
+++ pre-commit-hooks/php-syntax-check.sh
@@ -3,11 +3,13 @@
 # Checks all added and modified php files for syntax
 # if command-line php interpretator is available
 #
+against=HEAD #todo
 if command -v php > /dev/null ; then
-    errors=`echo $amfiles | sed 's/ /\n/g' \
-            | grep -E '.*[.]php$'  \
-            | xargs --no-run-if-empty -n 1 php -l  2>&1  \
-            | grep -vE '^No syntax errors detected' \
+    errors=`
+            git diff --cached $against --name-only --diff-filter=AM |
+            grep -E '.*[.]php$' |
+            xargs --no-run-if-empty -n 1 php -l  2>&1 |
+            grep -vE '^No syntax errors detected'
     `
     if [ ! -z "$errors" ]; then
         echo '!!!' PHP Syntax errors found:
